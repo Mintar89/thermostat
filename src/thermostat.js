716403a2267319ab10c3bb3190151b809f51566a
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-05-11 10:37:12 +0100

    Maximum temperature is 25 degrees when power saving mode is on

--- src/thermostat.js
+++ src/thermostat.js
@@ -5,6 +5,8 @@ class Thermostat {
     this.temperature = 20;
     this.MINIMUM_TEMPERATURE = 10;
     this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
   };
   
   getCurrentTemperature() {
@@ -12,15 +14,18 @@ class Thermostat {
   };
 
   up() {
-    this.temperature += 1 
-  };
+    if (this.isMaximumTemperature()) {
+      return;
+    }
+    this.temperature += 1;
+  }
 
   down() {
     if (this.isMinimumTemperature()) {
       return;
     }
     this.temperature -= 1 
-  };
+  }
 
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
@@ -28,14 +33,22 @@ class Thermostat {
 
   isPowerSavingModeOn() {
     return this.powerSavingMode === true;
-  };
+  }
 
   switchPowerSavingModeOff() {
     this.powerSavingMode = false;
-  };
+  }
   
   switchPowerSavingModeOn() {
     this.powerSavingMode = true;
-  };
+  }
+  
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+  
 
-};
+}
