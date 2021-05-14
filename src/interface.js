880740052e4a6cf57a1e0fe6abcb117a4b65cbe3
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-05-14 11:37:14 +0100

    Used Fetch API to get weather information from a weather API

--- src/interface.js
+++ src/interface.js
@@ -6,7 +6,28 @@ document.addEventListener('DOMContentLoaded', function() {
   
     const thermostat = new Thermostat();
       updateTemperature();
-  
+    
+    const displayWeather = (city) => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+      
+      fetch(url)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          document.querySelector('#current-temperature').innerText = data.main.temp;
+        })
+    } 
+    
+    displayWeather('London');
+
+    document.querySelector('#select-city').addEventListener('submit', (event) => {
+      event.preventDefault();
+      const city = document.querySelector('#current-city').value;
+        
+      displayWeather(city);
+    })
+    
     document.querySelector('#temperature-up').addEventListener('click', function() {
       thermostat.up();
       updateTemperature();
